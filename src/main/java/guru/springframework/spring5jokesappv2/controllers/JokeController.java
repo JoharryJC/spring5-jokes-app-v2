b26386fbbb6265a6a954c86086ee8c946850262e
JoharryJC <41237769+JoharryJC@users.noreply.github.com>
2021-03-15 22:03:17 +0100

    update JokeController

--- src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
+++ src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -1,11 +1,13 @@
 package guru.springframework.spring5jokesappv2.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import guru.springframework.spring5jokesappv2.services.JokeService;
 
+@Controller
 public class JokeController {
 
 	private final JokeService jokeService;
